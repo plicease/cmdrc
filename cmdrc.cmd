c3f3c1d23f7db50f1364d7e935fe3361c33136e3
Graham Ollis <plicease@cpan.org>
2025-08-29 21:34:11 -0600

    initial check in

---
+++ cmdrc.cmd
@@ -0,0 +1,3 @@
+@echo off
+rem "%CLINK_DIR%\clink.bat" inject --autorun profile ~\clink
+doskey /macrofile=%userprofile%\\cmd\\aliases.mac
---
+++ regenerate.pl
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+use feature qw( say );
+use Path::Tiny qw( path );
+use Capture::Tiny qw( capture_stdout );
+
+my $out = capture_stdout { system 'coreutils', '--list' };
+
+my %sp = (
+    ls => { ls => 'ls --color=auto -CF $*', 
+            dir => 'ls --color=auto -lF $*'
+    },
+    cp => 'cp -i $*',
+    rm => 'rm -i $*',
+    mv => 'mv -i $*',
+);
+
+foreach my $cmd (split /\s+/, $out) {
+    if($sp{$cmd}) {
+        if(ref $sp{$cmd} eq 'HASH') {
+            foreach my $alias (sort keys $sp{$cmd}->%*) {
+                my $exe  = $sp{$cmd}->{$alias};
+                say "$alias=coreutils $exe";
+            }
+        } else {
+            say "$cmd=coreutils $sp{$cmd}";
+        }
+    } else {
+        say "$cmd=coreutils $cmd \$*"
+    }
+}
